42d96ecb832529dd042a9c139a13261cabc8160d
jaytparekh7121994 <53175164+jaytparekh7121994@users.noreply.github.com>
2020-01-16 17:59:23 +0530

    return_list
    This file consists of return list. The return must be outside the loop. Just like while or for loop.

---
+++ fibonacci_return_list.py
@@ -0,0 +1,12 @@
+def fibonacci_series(number):
+    """ Prints the fibonacci_series uptill the number"""
+    result = []
+    a,b = 0,1
+    while a < number:
+	    result.append(a)
+	    a,b = b,a+b
+    return result # return statement must be outside the loop like while
+    
+fib = fibonacci_series(2000)
+
+print (fib) 	# Prints the list of fibonacci series
