71ef50cf9c3c7202fa1b40f43cf6e08264e46721
jaytparekh7121994 <53175164+jaytparekh7121994@users.noreply.github.com>
2020-01-17 18:09:43 +0530

    Added dash function and print comment in func
    The changes made in the code consists of dash function, under which the comment of function description is been printed by print(function_name.__doc__)

--- function_defaultArguements.py
+++ function_defaultArguements.py
@@ -1,6 +1,10 @@
 """ This code contains the example that the function has the "Default Arguement Values" eg; reminder ="Please try again!" or retries =4 """
+def dash():
+    print("\n\n",40*'-')
+    
 def ask_ok(prompt, retries=4, reminder='Please try again!'):
-    """ Enter the y, ye or yes or n, no, nop or nope """
+    """ Entering the y, ye or yes leads to Success.
+        Entering the n, no, nop or nope to Not Success. """
     while True:
         ok = input(prompt)
         if ok in ('y', 'ye', 'yes','Y','Ye','Yes'):
@@ -15,3 +19,5 @@ def ask_ok(prompt, retries=4, reminder='Please try again!'):
         print(reminder)
 
 ask_ok('Do You really want to quit?\n')
+dash()
+print(ask_ok.__doc__)
