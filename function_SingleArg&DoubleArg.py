022e11cdd95f4eca77ba8dd66e534084f0fccb7a
jaytparekh7121994 <53175164+jaytparekh7121994@users.noreply.github.com>
2020-01-17 16:30:29 +0530

    kind,*args and **keywords
    the kind is mandatory type of positional argument and *args and **keywords.
    *args => "Some strings", "some more strings"
    **keywords => Shopkeeper= "Jay" considered as a keyword

---
+++ function_SingleArg&DoubleArg.py
@@ -0,0 +1,16 @@
+def cheeseshop(kind, *arguments, **keywords):
+    """ Kind= Ham meaning compulsory argument
+        *arguments = Option, "Would...." and Rhyme
+        **keywords= Shopkeeper="Jay", empid= "345678", Order= "547"
+    """
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    for kw in keywords:
+        print(kw, ":", keywords[kw])
+Ham="Hamburger"
+Option="But Sir we have Cheese Burst"
+Rhyme="Hickory Dickory Dock a Mouse went up the clock"
+caller= cheeseshop(Ham,Option,"Would Mahi like a bit of a byte?",Rhyme,Shopkeeper="Jay" , empid="345678", Order ="547")
